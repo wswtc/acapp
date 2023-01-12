3f0a33405b4ade2c382a3da28cb8221dd9e21578
wswtc <woshiwtc@gmail.com>
2023-01-12 19:49:10 +0800

    error

--- game/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from game.views import index,play # 从game/views.py 里面调用index函数
-
-urlpatterns = [
-            path("", index, name="index"),
-            path("play/",play,name="play" )
-]
